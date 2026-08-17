cc51301b2d1bad722fa1abf33eb391517e024192
florian <sdflorian@unicesar.edu.co>
2026-08-16 22:27:53 -0500

    feat:implemente el metodo CREATE de Client

--- src/main/java/com/mycompany/biblioteca/Client.java
+++ src/main/java/com/mycompany/biblioteca/Client.java
@@ -1,5 +1,11 @@
 package com.mycompany.biblioteca;
 
 public class Client extends Person {
+
     String email;
+
+    public Client(String name, int id, int phone, String email) {
+        super(name, id, phone);
+        this.email = email;
+    }
 }
--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,13 +1,19 @@
-
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author florian
- */
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
+
+    static ArrayList<Client> clients = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    void CREATE(String name, int id, int phone, String email) {
+        Client newClient = new Client(name, id, phone, email);
+        clients.add(newClient);
     }
 }
